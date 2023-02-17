eddce1a5015bc6bfe6cc7c3b2757c63ad04af553
Junfeng <i@jacob953.com>
2023-02-17 17:11:59 +0800

    Revert "feat: Added user table and video table"

--- simple-demo/test/message_server_test.go
+++ simple-demo/test/message_server_test.go
@@ -3,11 +3,12 @@ package test
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/RaymondCode/simple-demo/controller"
 	"io"
 	"net"
 	"testing"
 	"time"
+
+	"github.com/RaymondCode/simple-demo/controller"
 )
 
 func TestMessageServer(t *testing.T) {
