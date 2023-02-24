69e8df77183b23e845ddd6798349f65a009875ea
Jacob953 <i@jacob953.com>
2023-02-24 10:23:12 +0800

    feat: create errcode
    Signed-off-by: Jacob953 <i@jacob953.com>

---
+++ internal/pkg/code/apiserver.go
@@ -0,0 +1,10 @@
+package code
+
+// iam-apiserver: user errors.
+const (
+	// ErrUserNotFound - 404: User not found.
+	ErrUserNotFound int = iota + 110001
+
+	// ErrUserAlreadyExist - 400: User already exist.
+	ErrUserAlreadyExist
+)
